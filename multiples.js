cbf867a2ab8069b019d6e3cf8d3da5af6ca9fb13
Filip Brouwers <filip.brouwers@gmail.com>
2022-09-11 09:44:51 +0200

    first commit from wsl

--- multiples.js
+++ multiples.js
@@ -1,5 +1,12 @@
-for(let i=0; i<10;i++) {
-    if(i%2 == 0) {
-        return i;
-    }
+
+const multiples = (maximum) => {
+  arr = [];
+  for(let i=0; i<maximum;i++) {
+      if(i%2 == 0) {
+          arr.push(i);
+      }
+  }
+  return arr;
 }
+
+console.log(multiples(10));
